2a0b4254c152b2533a67de9623803333d8afe606
ZhouXiang <1095971114@qq.com>
2022-06-06 01:00:12 +0800

    新建项目

---
+++ 源码/src/main/java/lex/Main.java
@@ -0,0 +1,13 @@
+package lex;
+
+/**
+ * @projectName: untitled
+ * @package: lex
+ * @className: Main
+ * @author: Zhou xiang
+ * @description: TODO
+ * @date: 2022/6/6 0:23
+ * @version: 1.0
+ */
+public class Main {
+}
