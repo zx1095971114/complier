0169f44872ee069c65b9d74e0e8c7036b037bdb6
ZhouXiang <1095971114@qq.com>
2023-06-29 14:24:03 +0800

    test

---
+++ C--Complier/程序演示/test.bat
@@ -0,0 +1,23 @@
+java -jar .\complier-1.0-SNAPSHOT-jar-with-dependencies.jar
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\00\\00.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\00\\38lex.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\00\\38gra.txt
+
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\01\\01.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\01\\38lex.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\01\\38gra.txt
+
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\02\\02.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\02\\38lex.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\02\\38gra.txt
+
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\07\\07.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\07\\38lex.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\07\\38gra.txt
+
+
+
+//现场的文件
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\test\\test38_r.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\test\\38lex_r.txt
+D:\\大学\\课程\\编译原理\\My大作业\\C--Complier\\程序演示\\test\\38gra_r.txt
